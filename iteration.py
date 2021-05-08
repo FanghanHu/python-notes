fc9d26bf41971fcfb0ba4f10c27386b96531882d
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-05-08 10:32:53 -0500

    update iteration notes

--- iteration.py
+++ iteration.py
@@ -1,3 +1,4 @@
+# %%
 #while loop:
 #else block will execute when exiting the loop, or if the while statement is false from the begining,
 # else block won't execute when breaking the loop with a break keyword.
@@ -8,12 +9,14 @@ while num < 10:
 else:
     print("wow, num is big now!")
 
+# %%
 #breaking out of while with the break keyword, return also break loops
 while True:
     num-=1
     if num < 4:
         break
 
+# %%
 nums = [1, 2, 3, 4, 5]
 #For loop:
 for num in nums:
@@ -23,16 +26,20 @@ for num in nums:
 else:
     print("else block in for loop")
 
+# %%
 #use the enumerate function to access the sequence number corralated with each element.
+nums = [1, 2, 3, 4, 5]
 for num in enumerate(nums):
     print(f"num: {num}")
 
+# %%
 #enumerate() takes a second optional argument that specifies the sequence starting number, 
 #which you could use, for example, to indicate the line number in a file, starting with 1 rather than the 0 default
+nums = [1, 2, 3, 4, 5]
 for num in enumerate(nums, 1):
     print(f"num: {num}")
 
-
+# %%
 #Inline loop structures: 
 #List Comprehension:  <result expression> for <loop variable> in <iterable> if <filter expression>    
 [n*n for n in range(1, 100) if n % 2 == 0] #All squares of numbers between 1~100 that is an even number
