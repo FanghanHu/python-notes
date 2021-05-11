9529b29b74351523070fc95d393d56a6e10ca9ab
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-05-10 22:22:38 -0500

    shuffle example

--- practice/shuffle.py
+++ practice/shuffle.py
@@ -2,14 +2,14 @@
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
-def navieSwap(arr, i):
-    "return an array of possible results by having item at i swap with every other item including itself"
-    result = []
+def navieStep(arr, i):
+    "simulate a step in a navie shuffle algorithm, return all possile result"
+    results = []
     for j in range(len(arr)):
         copy = arr.copy()
         swap(copy, i, j)
-        result.append(copy)
-    return result
+        results.append(copy)
+    return results
 
 def navieShuffle(arr):
     "return all possible results of a navie shuffle"
@@ -19,29 +19,83 @@ def navieShuffle(arr):
         # use temp to store result of each step
         temp = []
         for tempArr in result:
-            temp.extend(navieSwap(tempArr, i))
+            temp.extend(navieStep(tempArr, i))
         # replace results from last step with results from this step
         result = temp
     return result
 
+# run the simulation
 arr = ["A", "B", "C", "D", "E", "F"]
 result = navieShuffle(arr)
 
+# setup a dict to count the number of times each item ends up at each position
 count = {}
 for item in arr: 
     count[item] = [0] * len(arr)
 
+# counting
 for seq in result:
     for i, c in enumerate(seq):
         count[c][i] += 1
 
-#print column names
+# print column names
 line = "item"
 for i in range(len(arr)):
     line += f"\t{i}"
 print(line)
 
-#print number of times for each item ending up in each spot
+# print number of times for each item ending up in each spot
+for key in count:
+    line = f"{key} : "
+    for n in count[key]:
+        line += f"\t{n}"
+    print(line)
+# %%
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def fyStep(arr, i):
+    "simulate a step in Fisher-Yates shuffle, returning all possible results"
+    results = []
+    for j in range(0, i + 1):
+        copy = arr.copy()
+        swap(copy, j, i)
+        results.append(copy)
+    return results
+
+def fyShuffle(arr):
+    "return all possible result of a Fisher-Yates shuffle"
+    result = [arr]
+    # 0 is omitted on purpose as it will always produce the same result as last step
+    for i in range(len(arr) - 1, 0, -1):
+        temp = []
+        for tempArr in result:
+            temp.extend(fyStep(tempArr, i))
+        result = temp
+    return result
+
+# run the simulation
+arr = ["A", "B", "C", "D", "E", "F"]
+result = fyShuffle(arr)
+
+# setup a dict to count the number of times each item ends up at each position
+count = {}
+for item in arr: 
+    count[item] = [0] * len(arr)
+
+# counting
+for seq in result:
+    for i, c in enumerate(seq):
+        count[c][i] += 1
+
+# print column names
+line = "item"
+for i in range(len(arr)):
+    line += f"\t{i}"
+print(line)
+
+# print number of times for each item ending up in each spot
 for key in count:
     line = f"{key} : "
     for n in count[key]:
